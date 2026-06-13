b689a53b236af902cfc6dadf6048a88cd3e97895
Dailymemes111 <dailymemes141@gmail.com>
2026-06-13 22:28:56 +0300

    Update content.ts

--- src/data/content.ts
+++ src/data/content.ts
@@ -24,7 +24,7 @@ What really helped me stand out was developing my own unique "try not to laugh" 
       name: "Polymarket",
       logo: "/logos/polymarket-logo-1.png",
       logoClassName: "h-20 w-[122px]",
-    };
+    },
     {
       name: "Kling AI",
       logo: "/logos/kling-ai.png",
