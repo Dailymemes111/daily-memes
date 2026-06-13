9f90ebdfa3c6605bddbf3293f851ade5137dde1b
Dailymemes111 <dailymemes141@gmail.com>
2026-06-13 22:34:46 +0300

    Update content.ts

--- src/data/content.ts
+++ src/data/content.ts
@@ -27,7 +27,7 @@ What really helped me stand out was developing my own unique "try not to laugh" 
     },
     {
       name: "Kling AI",
-      logo: "/logos/klinga.png",
+      logo: "/logos/klinga.webp",
       logoClassName: "h-20 w-40",
     },
     {
